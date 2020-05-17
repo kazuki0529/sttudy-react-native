3b75d969d64fa217056896577e8e1954896fd269
kazuki0529 <luis.figo.xx.the.leopard@gmail.com>
2020-05-17 15:12:06 +0000

    メモリスト画面をコンポーネント化

--- App.tsx
+++ App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import MemoList from './src/components/MemoList'
 import AppBar from './src/components/AppBar'
-import CircleButton from './src/elements/CircleButton'
+import MemoListScreen from './src/screens/MemoListScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <AppBar />
-      <MemoList />
-      <CircleButton>＋</CircleButton>
+      <MemoListScreen />
     </View>
   );
 }
