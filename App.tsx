9952986933d69a2047b995131abcaf1079c3fe87
kazuki0529 <luis.figo.xx.the.leopard@gmail.com>
2020-05-17 14:41:51 +0000

    メモリストの画面をべた書き

--- App.tsx
+++ App.tsx
@@ -5,8 +5,27 @@ import BodyText from './src/elements/BodyText'
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello.</Text>
-      <BodyText>Hi!</BodyText>
+      <View style={styles.appBar}>
+        <View>
+          <Text style={styles.appBarTitle}>MEMOT</Text>
+        </View>
+      </View>
+
+      <View style={styles.memoList}>
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイディア</Text>
+          <Text style={styles.memoDate}>2020/05/17</Text>
+        </View>
+
+        <View style={styles.memoListItem}>
+          <Text style={styles.memoTitle}>講座のアイディア</Text>
+          <Text style={styles.memoDate}>2020/05/17</Text>
+        </View>
+      </View>
+
+      <View style={styles.memoAddButton}>
+        <Text style={styles.memoAddButtonTitle}>＋</Text>
+      </View>
     </View>
   );
 }
@@ -14,8 +33,69 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fffdf6',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  appBar: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: 78,
+    paddingTop: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#265366',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    zIndex: 10
+  },
+  appBarTitle: {
+    color: '#fff',
+    fontSize: 18,
+  },
+
+  memoList: {
+    width: '100%',
+    flex: 1,
+    paddingTop: 78
+  },
+  memoListItem: {
+    padding: 16,
+    backgroundColor: '#fff',
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4,
+  },
+  memoDate: {
+    fontSize: 12,
+    color: '#a2a2a2',
+  },
+
+  memoAddButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    backgroundColor: '#E31676',
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  memoAddButtonTitle: {
+    fontSize: 32,
+    lineHeight: 32,
+    color: '#fff',
   },
 });
